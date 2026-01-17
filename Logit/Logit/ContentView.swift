00d4b14a4e64affb53c414c7f7a6e36b8b0c32aa
LimJaeHyeon9298 <limjh9298@gmail.com>
2026-01-17 13:26:37 +0900

    Initial Commit

---
+++ Logit/Logit/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Logit
+//
+//  Created by 임재현 on 1/17/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+//print("hi")
---
+++ Logit/Logit/LogitApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogitApp.swift
+//  Logit
+//
+//  Created by 임재현 on 1/17/26.
+//
+
+import SwiftUI
+
+@main
+struct LogitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
