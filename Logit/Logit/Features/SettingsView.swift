289369f71047d92596fe8dd5fb04b99dab3bdd1c
LimJaeHyeon9298 <limjh9298@gmail.com>
2026-01-29 22:57:24 +0900

    [feat] #15 profile 이미지 클릭시 SettingsView fullScreenOver 작동 구현

---
+++ Logit/Logit/Features/SettingsView.swift
@@ -0,0 +1,14 @@
+//
+//  SettingsView.swift
+//  Logit
+//
+//  Created by 임재현 on 1/29/26.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
