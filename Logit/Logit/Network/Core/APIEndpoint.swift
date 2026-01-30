b6649a1fd9f3a7ee22421a93fd2d1d12e2727a31
LimJaeHyeon9298 <limjh9298@gmail.com>
2026-01-30 14:32:49 +0900

    [feat] #17 Auth API 관련 endpoint 정의

---
+++ Logit/Logit/Network/Core/APIEndpoint.swift
@@ -0,0 +1,46 @@
+//
+//  APIEndpoint.swift
+//  Logit
+//
+//  Created by 임재현 on 1/30/26.
+//
+
+import UIKit
+
+enum APIEndpoint {
+    // Auth
+    case googleLogin // 구글 로그인
+    case googleCallback // 구글 콜백
+    case refreshToken //  엑세스토큰 갱신
+    case logout // 로그아웃
+    
+    var path: String {
+        switch self {
+        case .googleLogin:
+            return "/api/v1/auth/google"
+        case .googleCallback:
+            return "/api/v1/auth/google/callback"
+        case .refreshToken:
+            return "/api/v1/auth/refresh"
+        case .logout:
+            return "/api/v1/auth/logout"
+        }
+    }
+    
+    var method: HTTPMethod {
+        switch self {
+        case .googleLogin, .googleCallback:
+            return .get
+        case .refreshToken, .logout:
+            return .post
+        }
+    }
+}
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+    case patch = "PATCH"
+}
