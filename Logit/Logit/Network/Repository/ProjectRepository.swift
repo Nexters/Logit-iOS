528c3fb81bd2fc7d32cd674b464d5a131b4d4c70
LimJaeHyeon9298 <limjh9298@gmail.com>
2026-01-30 21:09:13 +0900

    [feat] #26 프로젝트 생성 API 구현

--- Logit/Logit/Network/Repository/ProjectRepository.swift
+++ Logit/Logit/Network/Repository/ProjectRepository.swift
@@ -8,5 +8,23 @@
 import Foundation
 
 protocol ProjectRepository {
+    /// 프로젝트  생성
+    func createProject(request: CreateProjectRequest) async throws -> ProjectResponse
+}
+
+
+final class DefaultProjectRepository: ProjectRepository {
+    private let networkClient: NetworkClient
     
+    init(networkClient: NetworkClient = DefaultNetworkClient()) {
+        self.networkClient = networkClient
+    }
+    
+    // 프로젝트 생성
+    func createProject(request: CreateProjectRequest) async throws -> ProjectResponse {
+        return try await networkClient.request(
+            endpoint: ProjectEndpoint.createProject,
+            body: request
+        )
+    }
 }
